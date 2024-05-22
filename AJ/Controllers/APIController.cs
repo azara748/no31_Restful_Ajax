6a9a233fa1aaba4c076f0641ff502add97543673
31 <wanks894@gmail.com>
2024-05-22 21:01:00 +0800

    完成作業二

--- AJ/Controllers/APIController.cs
+++ AJ/Controllers/APIController.cs
@@ -55,24 +55,32 @@ namespace AJ.Controllers
         }
         public IActionResult Index4(Member a, IFormFile fil)
         {
-            string filePath = Path.Combine(_img.WebRootPath, "img", fil.FileName);
-            //using (var v = new FileStream(filePath, FileMode.Create))
-            //{
-            //    fil.CopyTo(v);
-            //}
-            byte[] bt;
-            fil.CopyTo(new FileStream(_img.WebRootPath + "/img/" + fil.FileName, FileMode.Create));
-            using (var v = new MemoryStream())
+            if (a.Email == null) a.Email = "無";
+            if (a.Age == null) a.Age = 99;
+            string s = "名字" + a.Name + "信箱" + a.Email + "年齡" + a.Age;
+            if (fil != null)
             {
-                fil.CopyTo(v);
-                bt = v.ToArray();
+                string filePath = Path.Combine(_img.WebRootPath, "img", fil.FileName);
+                //using (var v = new FileStream(filePath, FileMode.Create))
+                //{
+                //    fil.CopyTo(v);
+                //}
+                byte[] bt;
+                fil.CopyTo(new FileStream(_img.WebRootPath + "/img/" + fil.FileName, FileMode.Create));
+                using (var v = new MemoryStream())
+                {
+                    fil.CopyTo(v);
+                    bt = v.ToArray();
+                }
+                a.FileData = bt;
+                a.FileName = fil.FileName;
+                s += "檔案名" + fil.FileName + "大小" + fil.Length + "byte 類型" + fil.ContentType;
             }
-            a.FileData = bt;
-            a.FileName = fil.FileName;
+            else s += "無照片";
             _context.Members.Add(a);
             _context.SaveChanges();
-            return Content(fil.FileName + "," + fil.Length + "," + fil.ContentType, "text/plain", Encoding.UTF8);
-            //return Content("名字"+a.Name+"信箱"+a.Email+"年齡"+a.Age, "text/plain", Encoding.UTF8);
+            //return Content(fil.FileName + "," + fil.Length + "," + fil.ContentType, "text/plain", Encoding.UTF8);
+            return Content(s, "text/plain", Encoding.UTF8);
             //https://localhost:7045/API/Index4/1?name=aaa&sai=100
         }
         public IActionResult Index5([FromBody] josn接收 j)
